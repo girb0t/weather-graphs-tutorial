f4f77de8ffaa109158dfc57a6e00a34c52c250c4
Pei Xiong Liu <peixiong.liu@gmail.com>
2016-02-07 13:01:25 -0500

    Create weather reducer

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,14 @@
+import { FETCH_WEATHER } from '../actions/index';
+export default function(state = [], action) {
+  switch (action.type) {
+  case FETCH_WEATHER:
+  // Either of 2 following return statements work:
+
+    // NEVER use .push, which would mutate the state
+    // return state.concat([action.payload.data]);
+    debugger
+    return [ action.payload.data, ...state ]; // returns [city, city, city] NOT [city, [city, city]]
+
+  }
+  return state;
+}
